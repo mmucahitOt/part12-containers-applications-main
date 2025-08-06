1086101485ff335d63f4bc612735cb4d68a49e6a
mmucahitOt <m.mucahit0@gmail.com>
2025-08-06 14:00:49 +0300

    Exercise 12.18: Configure the Nginx server to be in front of todo-backend

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,5 +1,6 @@
 const MONGO_URL =
-  process.env.MONGO_URL || "mongodb://root:root1234@mongo:27017/todo-app";
+  process.env.MONGO_URL ||
+  "mongodb://root:root1234@mongo:27017/todo-app?authSource=admin";
 const REDIS_URL = process.env.REDIS_URL || "redis://:redis1234@redis:6379";
 
 module.exports = {
